25e52d67ca68180df376044c93545038853263c9
Ebanflo42 <eben.cowley42@gmail.com>
2017-10-13 23:21:44 -0700

    started parsing, since it will be needed for testing

--- homology-calculator.fs
+++ homology-calculator.fs
@@ -4,14 +4,13 @@ let bind (o : Option<'a>) (f : 'a -> Option<'b>) : Option<'b> =
     | None   -> None
     | Some x -> f x
 
-let rec drop (n : int) (list : List<'a>) =
-  List.tail (drop (n - 1) (List.tail list))
-
 let rec checkSuperSet (set : int list) (l1 : int) (Set : int list) (l2 : int) : bool =
-  if l2 < l1 then false elif List.take l1 Set = set then true else checkSuperSet set l1 (drop l1 Set) (l2 - l1)
+  if l2 < l1 then false elif List.take l1 Set = set then true else checkSuperSet set l1 (Set.[l1..]) (l2 - l1)
 
 let rec checkSubSet (Set : int list) (l1 : int) (set : int list) (l2 : int) : bool =
   checkSuperSet set l2 Set l1
+
+let rec maximum (list : int list) : int = List.fold max list.[0] (List.tail list)
 
 //DATA TYPES
 type HasseNode(Vertices : int list, Parents : HasseNode list, Children : HasseNode list) =
@@ -31,7 +30,7 @@ type HasseNode(Vertices : int list, Parents : HasseNode list, Children : HasseNo
     HasseNode.checkNodeChilds node && HasseNode.checkNodeParens node
 
 type SimplicialComplex(OrderedSets : int list list) =
-  member this.OrderedSets = OrderedSets
+  member this.OrderedSets : int list list = OrderedSets
 
   member this.getNthChainRank (n : int) : int =
     let rec numSets (sets : int list list) = match sets with
@@ -39,4 +38,40 @@ type SimplicialComplex(OrderedSets : int list list) =
                                           | (x :: xs) -> if x.Length = n then 1 + numSets xs else numSets xs
     numSets OrderedSets
 
-let _ = printf "A work in progress"
+  member this.Dimension : int = maximum (List.map (fun (list : int list) -> list.Length) OrderedSets)
+
+//PARSING
+let rec pow10 n = match n with
+                    | 0 -> 1
+                    | 1 -> 10
+                    | 2 -> 100
+                    | 3 -> 1000
+                    | 4 -> 10000
+                    | 5 -> 100000
+                    | 6 -> 1000000
+                    | 7 -> 10000000
+                    | 8 -> 100000000
+                    | 9 -> 1000000000
+                    | _ -> 10*pow10 (n - 1)
+
+let rec strToInt (n : int) (str : string) : int =
+  if n < 1 then 0 else
+    let rec getDigit c = match c with
+                           | '0' -> 0
+                           | '1' -> 1
+                           | '2' -> 2
+                           | '3' -> 3
+                           | '4' -> 4
+                           | '5' -> 5
+                           | '6' -> 6
+                           | '7' -> 7
+                           | '8' -> 8
+                           | '9' -> 9
+                           | _   -> -1
+    let digit = getDigit str.[0]
+    if digit = -1 then -1 else digit*(pow10 n) + strToInt (n - 1) str.[1..]
+
+//let parse (str : string) : HasseDiagram =
+
+let _ = let input = System.Console.ReadLine()
+        printfn "%i" (strToInt input)
