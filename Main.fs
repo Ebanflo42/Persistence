cb972ca813ca96ffff4041cc9ab5282287cff9de
Ebanflo42 <eben.cowley42@gmail.com>
2017-10-27 08:41:43 -0700

    implemented and tested extended euclidean algorith

--- Main.fs
+++ Main.fs
@@ -1,12 +1,25 @@
 module Main
 
 open Parser
+open Matrix
 
 let _ = while true do
 
           let input  = System.Console.ReadLine()
-          let output = parse2dIntList input
+          (*
+          let output =
+            input
+              |> parse2dIntList
+                |> fun (l : int list ist) -> [|for i in l -> [|for j in i -> j|]|]
+                  |> fun (m : int [] [])
+            *)
+          let output =
+            input
+              |> parseIntList
+                |> fun (l : int list) -> extEucAlg l.[0] l.[1]
 
+          printList [for i in output -> i]
+(*
           if output = [] then printfn "Please give valid input"
           else print2dList output
-          //print2dList [[1;2;3];[4;5;6];[7;8;9]]
+*)
--- Matrix.fs
+++ Matrix.fs
@@ -1,5 +1,21 @@
 module Matrix
 
+let rec extEucAlg (a : int) (b : int) : int [] =
+  let rec helper (r : int * int) (s : int * int) (t : int * int) =
+    match snd r with
+      | 0 -> [|fst r; fst s; fst t|]
+      | _ ->
+        let r1    = fst r
+        let r2    = snd r
+        let s2    = snd s
+        let t2    = snd t
+        let q     = r1 / r2
+        let nextr = r1 - q * r2
+        let nexts = fst s - q * s2
+        let nextt = fst t - q * t2
+        helper (r2, nextr) (s2, nexts) (t2, nextt)
+  helper (a, b) (0, 1) (1, 0)
+
 type Matrix(Elements : int [] [], Order : int) =
   member this.Elements = Elements
   member this.Order    = Order
@@ -8,23 +24,37 @@ type Matrix(Elements : int [] [], Order : int) =
     Array.forall (fun (a : 'a []) -> a.Length = Elements.[0].Length) Elements.[1..]
 
   member this.Transpose =
-    [|for i in 0..(Elements.Length - 1) -> [|for j in 0..(Elements.[0].Length) -> Elements.[j].[i]|]|]
-
-  member this.FindPivot =
+    let elems =
+      [|for i in 0..(Elements.Length - 1) -> [|for j in 0..(Elements.[0].Length) -> Elements.[j].[i]|]|]
+    Matrix(elems, Order)
+(*
+  member this.FindPivot : int * int =
 
     let rec getFstIndex (mat : int [] []) (index : int) =
       match mat with
         | [||] -> failwith "Couldn't find pivot"
         | _    ->
-          if Array.exists (fun a -> a = 0) mat.[0] then index
-          else getFstIndex mat.[1..] (index + 1)
+          match Array.tryFindIndex (fun x -> x = 0) mat.[0] with
+            | None   -> getFstIndex mat.[1..] (index + 1)
+            | Just i -> (index, i)
 
-    getFstIndex this.Transpose 0
-(*
-  static member improvePivot =
+    getFstIndex this.Elements 0
+
+  member this.improvePivot =
 
     let rec gcd a b =
       if b = 0
       then abs a
       else gcd b (a % b)
+
+    let indices = this.FindPivot
+    let row     = Elements.[fst indices]
+    let elem    = row.[snd indices]
+
+    let rec findGCD (arr : int []) =
+      match arr with
+      | []        -> failwith "Couldn't find Smith normal form"
+      | (x :: xs) ->
+        if x % elem != 0 then gcd x elem
+        else findGCD xs
 *)
