3422348f9fe20553883f86605f1b04de364420a4
Ebanflo42 <eben.cowley42@gmail.com>
2017-11-08 13:33:10 -0800

    I'm really sure smith normal form is done this time

--- Main.hs
+++ Main.hs
@@ -7,7 +7,7 @@ import SimplicialComplex
 
 main = do
   let matrix :: Matrix Int
-      matrix = Matrix [[2,3,5,7,11],[13,(-17),19,(-23),29],[(-31),37,(-41),43,(-47)],[53,(-59),61,(-67),71],[(-73),79,(-83),89,0]] 4 0
+      matrix = initializeMatrix 4 [[2,3,5,7,11],[13,(-17),19,(-23),29],[(-31),37,(-41),43,(-47)],[53,(-59),61,(-67),71],[(-73),79,(-83),89,0]]
   putStrLn "before: "
   putStrLn $ toString matrix
   let after = eliminateEntries matrix
--- Matrix.hs
+++ Matrix.hs
@@ -4,11 +4,16 @@ import Util
 import Data.List
 import Control.Parallel
 
-data Matrix a = Matrix [[a]] a
+data Matrix a = Matrix [[a]] a Int Int
 
-getElems (Matrix elems _) = elems
-getOrder (Matrix _ order) = order
---getNumColSwitches (Matrix _ _ switches) = switches
+getElems (Matrix elems _ _ _) = elems --elements of the matrix
+getOrder (Matrix _ order _ _) = order --modulus of the elements
+getLength (Matrix _ _ len _)  = len   --the minimum of the column and row length of the matrix minus one (maximum val for index)
+getIndex (Matrix _ _ _ index) = index --index of the pivot that was found last
+
+initializeMatrix :: Integral a => a -> [[a]] -> Matrix a
+initializeMatrix order []    = error "Nobody likes an empty matrix"
+initializeMatrix order elems = Matrix elems order (min (length $ head elems) (length elems) - 1) 0
 
 toString :: Matrix Int -> String 
 toString matrix =
@@ -36,37 +41,17 @@ findFstNotAllZeroCol i (r:rs) =
   if any (\n -> n /= 0) r then Just i
   else findFstNotAllZeroCol (i + 1) rs
 
---first arg is the row index, second arg is the first column with a zero entry
---finds the first non-zero element in the first column with a zero entry
---returns the row index and maybe the value at the index
-{-
-findPivot :: Integral a => Int -> Int -> [[a]] -> (Int, Maybe a)
-findPivot row col []     = (row, Nothing)
-findPivot row col (r:rs) = 
-  let elem = r !! col in
-  if elem == 0 then findPivot (row + 1) col rs
-  else (row, Just elem)
--}
-
+--looks for the first non-zero diagonal entry
 findPivot :: Integral a => Int -> [[a]] -> Maybe (Int, a)
 findPivot _ []     = Nothing
 findPivot n (r:rs) =
-  let elem = head r in
+  let elem = r !! n in
   if elem /= 0 then Just (n, elem)
   else findPivot (n + 1) rs
 
---Finds all non-divisible elements in a column and their indices
+--Finds all non-divisible elements in a row and their indices
+--first arg is current index, second is the element for which divisibility is being checked
 findNonDivisible :: Integral a => Int -> a -> [a] -> [(Int, a)]
-{--
-findNonDivisible _ _ _ []             = []
-findNonDivisible row col pivot (r:rs) =
-  let rest = findNonDivisible (row + 1) col pivot rs in
-    case par rest (r !! col) of
-      0                      -> rest
-      x | x == pivot         -> rest
-      x | x `mod` pivot == 0 -> rest
-      x                      -> (row, x) : rest
---}
 findNonDivisible index pivot []     = []
 findNonDivisible index pivot (x:xs) =
   case x of
@@ -75,7 +60,9 @@ findNonDivisible index pivot (x:xs) =
     a | a `mod` pivot == 0 -> findNonDivisible (index + 1) pivot xs
     _                      -> (index, x) : (findNonDivisible (index + 1) pivot xs)
 
-
+--Given two indices, breaks up a matrix into everthing before the first index,
+--the row at the first index, everything between the first and second indices,
+--the row at the second index, and everthing after that
 splitMatrix :: Int -> ([[a]], [a], [[a]], [a], [[a]]) -> Int -> Int -> [[a]] -> ([[a]], [a], [[a]], [a], [[a]])
 splitMatrix _ result _ _ [] = result
 splitMatrix current (mat1, row1, mat2, row2, mat3) i j (r:rs)
@@ -86,82 +73,92 @@ splitMatrix current (mat1, row1, mat2, row2, mat3) i j (r:rs)
   | current == j = splitMatrix (current + 1) (mat1, row1, mat2, r, mat3) i j rs
   | otherwise    = splitMatrix (current + 1) (mat1, row1, mat2, row2, r:mat3) i j rs
 
+--first two args are the indices of the rows undergoing the operation
+--3rd and 4th are the coefficients multiplying the two rows to give the row at the pivot index
+rowOperation :: Integral a => Int -> Int -> a -> a -> a -> a -> Matrix a -> [[a]]
+rowOperation pIndex index pcoeff1 pcoeff2 coeff1 coeff2 (Matrix elems _ _ _)
+  | pIndex == index     = error "Tried to perform a row operation on a single row"
+  | pIndex < index      =
+    let first  = take pIndex elems
+        second = drop (pIndex + 1) (take index elems)
+        third  = drop (index + 1) elems
+        row1   = elems !! pIndex
+        row2   = elems !! index in
+    first ++ (((pcoeff1 `mul` row1) `add` (pcoeff2 `mul` row2)):second) ++ (((coeff1 `mul` row1) `add` (coeff2 `mul` row2)):third)
+  | otherwise           =
+    let first  = take index elems
+        second = drop (index + 1) (take pIndex elems)
+        third  = drop (pIndex + 1) elems
+        row1   = elems !! index
+        row2   = elems !! pIndex in
+    first ++ (((coeff1 `mul` row1) `add` (coeff2 `mul` row2)):second) ++ (((pcoeff1 `mul` row1) `add` (pcoeff2 `mul` row2)):third)
+
 --first argument is the index of the column of interest
 --second arg is the row index of the pivot and the value of the pivot
 --third arg is the other element's index and it's value
 improvePivot :: Integral a => Int -> a -> [(Int, a)] -> Matrix a -> Matrix a
-improvePivot pIndex pivot [(index, elem)] (Matrix elems ord) = 
+improvePivot _ _ [] m                                            = m
+improvePivot pIndex pivot [(index, elem)] (Matrix elems ord l n) =
   let gcdTriple = extEucAlg pivot elem --gcd + bezout coefficients
       gcd       = one gcdTriple
       q1        = pivot `div` gcd
       q2        = elem `div` gcd
-      split     = splitMatrix 0 ([],[],[],[],[]) pIndex index elems
-      row1      = five2 split
-      row2      = five4 split
+      newElems  = rowOperation pIndex index (thr gcdTriple) (two gcdTriple) (-q2) q1 (Matrix elems ord l n) in
 
-      modulo    = if ord == 0 then id
-                  else (\n -> n `mod` ord)
-{--
-      newRow1   = modulo (((thr gcdTriple) `mul` row1) `add` ((two gcdTriple) `mul` row2))
-      newRow2   = modulo (((-q2) `mul` row1) `add` (q1 `mul` row2))
-
-      newElems  = (five1 split) ++ (newRow1 : (five3 split)) ++ (newRow2 : (five5 split))
-      --}
-      nonDivis  = findNonDivisible 0 col pivot elems in
-
-  Matrix newElems ord
+  Matrix newElems ord l n
 
 improvePivot col pair (nonDivis : rest) matrix =
   improvePivot col pair rest (improvePivot col pair [nonDivis] matrix)
 
---returns improved matrix and the index of the column to be eliminated
---TODO: fix column switching slowness
-findAndImprovePivot :: Integral a => Matrix a -> (Int, Int, a, Matrix a)
-findAndImprovePivot (Matrix elems ord x) =
-  {--
-  let column    = case findFstZeroCol elems of
-                    Nothing -> 0
-                    Just i  -> i
-      pivotData = findPivot 0 column elems in
-  
-  case snd pivotData of
-    Nothing -> findAndImprovePivot $ Matrix (switchCols column (length elems - 1 - x) elems) ord (x + 1)
-    Just p  ->
-      let pIndex = fst pivotData in
-      case findNonDivisible pIndex column p (drop pIndex elems) of
-        [] -> (column, pIndex, p, Matrix elems ord x)
-        xs -> (column, pIndex, p, improvePivot column (pIndex, p) xs (Matrix elems ord x))
-  --}
-  case findPivot 0 elems of
-    Nothing     -> findAndImprovePivot $ Matrix ((tail elems) ++ [head elems]) ord
-    Just (n, e) ->
+--returns index of column to be eliminated, pivot, and the improved matrix
+findAndImprovePivot :: Integral a => Matrix a -> (Int, a, Matrix a)
+findAndImprovePivot (Matrix elems ord l num) =
+  case findPivot num (drop num elems) of
+    Nothing     -> error "Couldn't find pivot"
+      {--
+      case num of
+        n | n == length elems -> (0, 0, Matrix elems ord num)
+        n                     -> findAndImprovePivot $ Matrix ((tail elems) ++ [head elems]) ord (n + 1)
+      --}
+    Just (i, p) ->
+      (i, p, improvePivot i p (findNonDivisible 0 p (elems !! i)) (Matrix elems ord l i))
 
-
+--calls findAndImprovePivot then eliminates the appropriate column
 eliminateEntries :: Integral a => Matrix a -> Matrix a
 eliminateEntries matrix = 
   let improved = findAndImprovePivot matrix
-      col      = four1 improved
-      mat      = four4 improved
+      pIndex   = one improved
+      mat      = thr improved
       elems    = getElems mat
-      pRow     = elems !! (four2 improved)
-      pivot    = four3 improved
+      pRow     = elems !! pIndex
+      pivot    = two improved
       ord      = getOrder mat
       modulo   = if ord == 0 then id
                  else (map . map) (\n -> n `mod` ord)
-      newElems = map (\row -> let e = row !! col in
+      newElems = map (\row -> let e = row !! pIndex in
                               if e == pivot then row
                               else
                                 ((-e) `div` pivot) `mul` pRow `add` row)
                    (getElems mat) in
-  Matrix (modulo newElems) (getOrder mat)
+  Matrix (modulo newElems) (getOrder mat) (getLength mat) (getIndex mat)
 
-getMinimalEntries :: Integral a => Bool -> Matrix a -> Matrix a
-getMinimalEntries b (Matrix elems order num) =
-  let elim = eliminateEntries (Matrix elems order num) in
+getMinimalEntries :: Integral a => Matrix a -> Matrix a
+getMinimalEntries (Matrix elems order len n) =
+  let elim = eliminateEntries (Matrix elems order len n)
+      tr   = eliminateEntries $ Matrix (transpose $ getElems elim) order len (getIndex elim) in
+  case getIndex tr of
+    i | i == len -> Matrix (transpose $ getElems tr) order len i
+    i            -> getMinimalEntries $ Matrix (transpose $ getElems tr) order len i
+{--
   case getElems elim of
     m | m == elems ->
       let tr = transpose elems in
-      case eliminateEntries (Matrix tr order num) of
-        Matrix tr o n -> if b then Matrix (transpose tr) o n else Matrix tr o n
-        m             -> getMinimalEntries True m
-    _               -> getMinimalEntries b elim
+      case eliminateEntries (Matrix tr order n) of
+        Matrix es o  n | es == tr -> if b then Matrix (transpose tr) o n else Matrix tr o n
+        m                         -> getMinimalEntries True m
+    m              ->
+      case getIndex elim of
+        i | i == length elems - 1 -> if b then Matrix (transpose m) order i
+                                     else Matrix m order i
+        i                         -> getMinimalEntries False (Matrix m order i)
+--}
