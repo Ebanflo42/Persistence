0e76079a04291a5fc32157baef6bdcee3ad0f566
Ebanflo42 <eben.cowley42@gmail.com>
2017-11-13 22:35:18 -0800

    algo is working for some test cases

--- Matrix.hs
+++ Matrix.hs
@@ -11,107 +11,46 @@ getOrder (Matrix _ order _ _) = order --modulus of the elements
 getIndex (Matrix _ _ index _) = index --index of the current pivot
 getMaxIndex (Matrix _ _ _ m)  = m
 
+--given the *order* of the group and the elements of the matrix
+--calculates the maximum pivot index and sets initial pivot index to zero
 initializeMatrix :: Integral a => a -> [[a]] -> Matrix a
 initializeMatrix order elems =
   Matrix elems order 0 ((min (length $ elems !! 0) (length elems)) - 1)
 
+--increments the pivot index
+incrementIndex :: Integral a => Matrix a -> Matrix a
+incrementIndex (Matrix a b i c) = Matrix a b (i + 1) c
+
+--shows the rows of an integer matrix separated by new lines
+--as well as the modulus of the matrix
 toString :: Matrix Int -> String 
 toString matrix =
   let mat = flatten $ map (\row -> '\n':(flatten $ map (\e -> ' ':(show e)) row)) (getElems matrix) in
   mat ++ "\nmodulo " ++ (show $ getOrder matrix)
 
---switch two columns, second must be greater than first
---switchElems found in Util
-switchCols :: Int -> Int -> [[a]] -> [[a]]
-switchCols col1 col2 = (transpose . (switchElems col1 col2) . transpose)
-
---finds the first column with a zero entry
-findFstZeroCol :: Integral a => [[a]] -> Maybe Int
-findFstZeroCol []     = Nothing
-findFstZeroCol (r:rs) =
-  let rest  = findFstZeroCol rs
-      index = elemIndex 0 r in
-  case seq index (par rest index) of
-    Nothing -> findFstZeroCol rs
-    Just i  -> Just i
-
-findFstNotAllZeroCol :: Integral a => Int -> [[a]] -> Maybe Int
-findFstNotAllZeroCol _ []     = Nothing
-findFstNotAllZeroCol i (r:rs) =
-  if any (\n -> n /= 0) r then Just i
-  else findFstNotAllZeroCol (i + 1) rs
-
---looks for the first non-zero diagonal entry
-findPivot :: Integral a => Int -> [[a]] -> Maybe (Int, a)
-findPivot _ []     = Nothing
-findPivot n (r:rs) =
-  let elem = r !! n in
-  if elem /= 0 then Just (n, elem)
-  else findPivot (n + 1) rs
-
---Finds all non-divisible elements in a row and their indices
---first arg is current index, second is the element for which divisibility is being checked
-findNonDivisible :: Integral a => Int -> a -> [a] -> [(Int, a)]
-findNonDivisible index pivot []     = []
-findNonDivisible index pivot (x:xs) =
-  case x of
-    0                      -> findNonDivisible (index + 1) pivot xs
-    a | a == pivot         -> findNonDivisible (index + 1) pivot xs
-    a | a `mod` pivot == 0 -> findNonDivisible (index + 1) pivot xs
-    _                      -> (index, x) : (findNonDivisible (index + 1) pivot xs)
-
---Given two indices, breaks up a matrix into everthing before the first index,
---the row at the first index, everything between the first and second indices,
---the row at the second index, and everthing after that
-splitMatrix :: Int -> ([[a]], [a], [[a]], [a], [[a]]) -> Int -> Int -> [[a]] -> ([[a]], [a], [[a]], [a], [[a]])
-splitMatrix _ result _ _ [] = result
-splitMatrix current (mat1, row1, mat2, row2, mat3) i j (r:rs)
-  | j < i        = splitMatrix current (mat1, row1, mat2, row2, mat3) j i (r:rs)
-  | current < i  = splitMatrix (current + 1) (r:mat1, row1, mat2, row2, mat3) i j rs
-  | current == i = splitMatrix (current + 1) (mat1, r, mat2, row2, mat3) i j rs
-  | current < j  = splitMatrix (current + 1) (mat1, row1, r:mat2, row2, mat3) i j rs
-  | current == j = splitMatrix (current + 1) (mat1, row1, mat2, r, mat3) i j rs
-  | otherwise    = splitMatrix (current + 1) (mat1, row1, mat2, row2, r:mat3) i j rs
-
---first two args are the indices of the rows undergoing the operation
---3rd and 4th are the coefficients multiplying the two rows to give the row at the pivot index
-rowOperation :: Integral a => Int -> Int -> a -> a -> a -> a -> Matrix a -> [[a]]
-rowOperation pIndex index pcoeff1 pcoeff2 coeff1 coeff2 (Matrix elems _ _ _)
-  | pIndex == index     = error "Tried to perform a row operation on a single row"
-  | pIndex < index      =
-    let first  = take pIndex elems
-        second = drop (pIndex + 1) (take index elems)
-        third  = drop (index + 1) elems
-        row1   = elems !! pIndex
-        row2   = elems !! index in
-    first ++ (((pcoeff1 `mul` row1) `add` (pcoeff2 `mul` row2)):second) ++ (((coeff1 `mul` row1) `add` (coeff2 `mul` row2)):third)
-  | otherwise           =
-    let first  = take index elems
-        second = drop (index + 1) (take pIndex elems)
-        third  = drop (pIndex + 1) elems
-        row1   = elems !! index
-        row2   = elems !! pIndex in
-    first ++ (((coeff1 `mul` row1) `add` (coeff2 `mul` row2)):second) ++ (((pcoeff1 `mul` row1) `add` (pcoeff2 `mul` row2)):third)
-
-{--}
-findRow :: Integral a => Int -> Int -> [[a]] -> Maybe (Either Int (Int, Int))
-findRow i max [r]     =
+--first argument is alooping index, second argument is the upper bound for that index
+--if a pivot row isn't found return nothing
+--if it is found and the diagonal element is non-zero return the pivot index
+--if it is found and is zero return the row index and the column index of the first non-zero element in the row
+pivotHelper :: Integral a => Int -> Int -> [[a]] -> Maybe (Either Int (Int, Int))
+pivotHelper i max [r]     =
   if i >= max then Nothing
   else if r !! i == 0 then
     case findIndex (\n -> n /= 0) r of
       Nothing -> Nothing
       Just x  -> Just $ Right (i, x)
   else Just $ Left i
-findRow i max (r:rs) =
+pivotHelper i max (r:rs) =
   if r !! i == 0 then
     case findIndex (\n -> n /= 0) r of
-      Nothing -> findRow (i + 1) max rs
+      Nothing -> pivotHelper (i + 1) max rs
       Just x  -> Just $ Right (i, x)
   else Just $ Left i
 
+--calls pivot helper, rearranges the matrix if necessary, returns the matrix pair with its pivot
 choosePivot :: Integral a => Matrix a -> (Maybe a, Matrix a)
 choosePivot (Matrix elems ord i max) =
-  case findRow i max (drop i elems) of
+  case pivotHelper i max (drop i elems) of
     Nothing             -> (Nothing, Matrix elems ord i max)
     Just (Left x)       -> (Just $ elems !! x !! x, Matrix elems ord x max)
     Just (Right (x, y)) ->
@@ -119,6 +58,9 @@ choosePivot (Matrix elems ord i max) =
           newElems = map (switchElems x y) elems in
       (Just pivot, Matrix newElems ord x max)
 
+--given the index of the pivot, a pair, and a row of a matrix
+--the first part of the pair is the gcd, bezout coefficients, and quotients
+--second part of the pair is the index of the element to which that applies
 colOperationHelper :: Integral a => Int -> ((a, a, a, a, a), Int) -> [a] -> [a]
 colOperationHelper pIndex ((gcd, coeff1, coeff2, q1, q2), index) row
   | index == pIndex = error "Tried to do column operation on a single column."
@@ -137,10 +79,12 @@ colOperationHelper pIndex ((gcd, coeff1, coeff2, q1, q2), index) row
         elem2  = row !! pIndex in
     first ++ ((coeff1*elem2 + coeff2*elem1) : second) ++ (((-q2)*elem1 + q1*elem2) : third)
 
+--given the pivot of a matrix and the matrix itself to improve that row with column operations
 improvePivot :: Integral a => (a, Matrix a) -> Matrix a
 improvePivot (pivot, Matrix elems ord pIndex max) =
-  let row          = elems !! pIndex in
-  case indexAndElem (\n -> n `mod` pivot /= 0) row of
+  let row          = elems !! pIndex
+      nonDivis     = indexAndElem (\n -> n /= 0 && n `mod` pivot /= 0) row in
+  case nonDivis of
     Nothing     -> Matrix elems ord pIndex max
     Just (n, i) ->
       let gcdTriple    = extEucAlg pivot n
@@ -149,6 +93,7 @@ improvePivot (pivot, Matrix elems ord pIndex max) =
           newElems     = map (colOperationHelper pIndex transform) elems in
       improvePivot (newElems !! pIndex !! pIndex, Matrix newElems ord pIndex max)
 
+--given a matrix whose pivot row has been improved, eliminates the entries in that row
 eliminateEntries :: Integral a => Matrix a -> Matrix a
 eliminateEntries (Matrix elems ord pIndex max) =
   let row      = elems !! pIndex
@@ -157,6 +102,7 @@ eliminateEntries (Matrix elems ord pIndex max) =
       newElems = map (\row -> mapWithIndex (\i elem -> if i == pIndex then elem else elem - (coeffs !! i)*(row !! pIndex)) row) elems in
   Matrix newElems ord pIndex max
 
+--gets the smith normal form of a matrix
 getSmithNormalForm :: Integral a => Matrix a -> Matrix a
 getSmithNormalForm matrix =
   case choosePivot matrix of
@@ -164,12 +110,12 @@ getSmithNormalForm matrix =
     (Just p, mat) ->
       if p == 0 then error "Pivot was found to be zero."
       else
-        let m      = improvePivot (p, mat)
-            elim1  = eliminateEntries m
-            pIndex = getIndex elim1
-            ord    = getOrder elim1
-            elems  = getElems elim1
-            tr     = Matrix (transpose $ getElems elim1) ord pIndex (getMaxIndex elim1)
-            elim2  = eliminateEntries $ improvePivot (p, tr) in
-        getSmithNormalForm elim2
---}
+        let improved = improvePivot (p, mat)
+            elim1    = eliminateEntries improved
+            pIndex   = getIndex elim1
+            ord      = getOrder elim1
+            elems    = getElems elim1
+            tr       = Matrix (transpose elems) ord pIndex (getMaxIndex elim1) in
+        case choosePivot tr of
+          (Nothing, _)  -> (getSmithNormalForm . incrementIndex) tr
+          (Just p, mat) -> (getSmithNormalForm . incrementIndex . eliminateEntries . improvePivot) (p, mat)
